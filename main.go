55836638799507dc0d4059e2f1f1114d57287a01
Chupriarti <wallx13@gmail.com>
2023-08-08 00:59:12 +0300

    add basic calculation

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+  "bufio"
+  "fmt"
+  "os"
+  "strings"
+  "strconv"
+)
 
 func main() {
-	fmt.Println("Hello Kata!")
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Println("# Welcome to KATA calculator! What to calc?")
+	text, _ := reader.ReadString('\n')
+	text = strings.Replace(text, "\n", "", -1)
+	inputParts := strings.Fields(text)
+
+	operand1, _ := strconv.Atoi(inputParts[0])
+	operand2, _ := strconv.Atoi(inputParts[2])
+	operator := inputParts[1]
+	var result int
+
+	switch operator {
+	case "+":
+		result = operand1 + operand2
+	case "-":
+		result = operand1 - operand2
+	case "*":
+		result = operand1 * operand2
+	case "/":
+		result = int(operand1 / operand2)
+	}
+
+	fmt.Println("Result: ")
+	fmt.Println(result)
 }
