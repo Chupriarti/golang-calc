fd3ba0750eaa2346e5d3d6964c37fe2bb8de06c8
Chupriarti <wallx13@gmail.com>
2023-08-09 08:22:09 +0500

    add int to roman func

--- main.go
+++ main.go
@@ -40,3 +40,33 @@ func main() {
 	fmt.Println("Result: ")
 	fmt.Println(result)
 }
+
+func intToRoman(number int) string {
+	conversions := []struct {
+		value int
+		digit string
+	}{
+		{1000, "M"},
+		{900, "CM"},
+		{500, "D"},
+		{400, "CD"},
+		{100, "C"},
+		{90, "XC"},
+		{50, "L"},
+		{40, "XL"},
+		{10, "X"},
+		{9, "IX"},
+		{5, "V"},
+		{4, "IV"},
+		{1, "I"},
+	}
+
+	var roman strings.Builder
+	for _, conversion := range conversions {
+		for number >= conversion.value {
+			roman.WriteString(conversion.digit)
+			number -= conversion.value
+		}
+	}
+	return roman.String()
+}
