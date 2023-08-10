9dd36ccd833366b45f72831db6bfe4278701bcf3
Chupriarti <wallx13@gmail.com>
2023-08-10 15:24:10 +0500

    add roman to int func

--- main.go
+++ main.go
@@ -70,3 +70,30 @@ func intToRoman(number int) string {
 	}
 	return roman.String()
 }
+
+func romanToInt(s string) int {
+
+	var RomanNumerals = map[rune]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	sum := 0
+	greatest := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		letter := s[i]
+		num := RomanNumerals[rune(letter)]
+		if num >= greatest {
+			greatest = num
+			sum = sum + num
+			continue
+		}
+		sum = sum - num
+	}
+	return sum
+}
